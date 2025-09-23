06eabf03c63d45e7eafa2d950557db9e8c19f951
Benoit Thierry <benoit.thierry@umontreal.ca>
2025-09-23 12:39:25 -0400

    Complete OxWalk processing, multiproc version. Ready for deploying to CC

--- sensedoc/ETL/top_step.py
+++ sensedoc/ETL/top_step.py
@@ -30,12 +30,16 @@ If TARGET_ROOT_FOLDER not provided, will default to test data folder.
 
 import os
 import sys
+import re
+import io
 import logging
 import pandas as pd
 from tempfile import NamedTemporaryFile
 from sqlalchemy import create_engine, text
+from time import perf_counter
 import multiprocessing as mp
 from itertools import starmap
+from contextlib import redirect_stdout
 
 # stepcount module, see https://github.com/OxWearables/stepcount
 from stepcount import stepcount
@@ -83,17 +87,17 @@ def execute_alter_top(city_code:str, wave:int):
     # ToP 1 minute table new columns
     stmt_ddl_top1min = f"""
         ALTER TABLE {target_schema}.{target_table1min}
-            ADD COLUMN IF NOT EXISTS steps INTEGER,
-            ADD COLUMN IF NOT EXISTS enmo FLOAT,
-            ADD COLUMN IF NOT EXISTS steps_adj INTEGER,
-            ADD COLUMN IF NOT EXISTS enmo_adj FLOAT            
+            ADD COLUMN IF NOT EXISTS steps SMALLINT,
+            ADD COLUMN IF NOT EXISTS enmo REAL,
+            ADD COLUMN IF NOT EXISTS steps_adj SMALLINT,
+            ADD COLUMN IF NOT EXISTS enmo_adj REAL            
     """
 
     with top_con.begin() as conn:
         conn.execute(text(stmt_ddl_top1min))
 
 
-def single_step_produce(city_code:str, wave:int, axl_elite_filename:str, dst_dir):
+def single_step_produce(city_code:str, wave:int, axl_elite_filename:str, dst_dir=None):
     """
     Processing of a single participant's SD data.
     Load AXL data from elite file subfolder. Reformat AXL CSV to match stepcount
@@ -119,6 +123,7 @@ def single_step_produce(city_code:str, wave:int, axl_elite_filename:str, dst_dir
     - Additional metrics (TODO: add description)
     """
     logger.debug(f'PID {mp.current_process().pid}: processing {os.path.basename(axl_elite_filename)}')
+    c0 = perf_counter()
 
     # A bit of checking needed here:
     if wave not in waves:
@@ -161,54 +166,81 @@ def single_step_produce(city_code:str, wave:int, axl_elite_filename:str, dst_dir
         return (city_code, wave, os.path.basename(axl_elite_filename), 0, f'Error formatting AXL ({e})')
 
     # Compute steps
-    out_folder = os.path.join(scratch_folder, "outputs_stepcount", f"w{wave}")
-    sys.argv += [axl_filename, "-q", 
-                 "-d", "cpu", 
-                 "-t", "ssl", 
-                 "--txyz", "utcdate,x,y,z", 
-                 "-o", out_folder]
-    stepcount.main()
+    try:
+        if dst_dir is None:
+            dst_dir = scratch_folder
+        out_folder = os.path.join(dst_dir, "outputs_stepcount", f"w{wave}")
+        sys.argv += [axl_filename, "-q", 
+                    "-d", "cpu", 
+                    "-t", "ssl", 
+                    "--txyz", "utcdate,x,y,z", 
+                    "-o", out_folder]
+        with redirect_stdout(io.StringIO()) as f:
+            stepcount.main()
+    except Exception as e:
+        logger.error(f'Unable to compute steps from {os.path.basename(axl_elite_filename)}, skipping')
+        return (city_code, wave, os.path.basename(axl_elite_filename), 0, f'Error computing steps ({e})')
+
 
     # Get minute and minuteAdjusted files
-    rootname = os.path.splitext(os.path.basename(axl_filename))[0]
-    stepCnt_filename = os.path.join(out_folder, rootname, f'{rootname}-Minutely.csv.gz')
-    stepCntAdj_filename = os.path.join(out_folder, rootname, f'{rootname}-MinutelyAdjusted.csv.gz')    
-    stepCnt = pd.read_csv(stepCnt_filename).drop(columns="Filename")
-    stepCnt['interact_id'] = int(interact_id)
-    stepCnt['sd_id'] = int(sd_id)
-    stepCnt['utcdate'] = pd.to_datetime(stepCnt['Time'], format="%Y-%m-%d %H:%M:%S", utc=True)
-    stepCntAdj = pd.read_csv(stepCntAdj_filename).drop(columns="Filename")
-    stepCntAdj['interact_id'] = int(interact_id)
-    stepCntAdj['sd_id'] = int(sd_id)
-    stepCntAdj['utcdate'] = pd.to_datetime(stepCnt['Time'], format="%Y-%m-%d %H:%M:%S", utc=True)
+    try:
+        rootname = os.path.splitext(os.path.basename(axl_filename))[0]
+        stepCnt_filename = os.path.join(out_folder, rootname, f'{rootname}-Minutely.csv.gz')
+        stepCntAdj_filename = os.path.join(out_folder, rootname, f'{rootname}-MinutelyAdjusted.csv.gz')    
+        stepCnt = pd.read_csv(stepCnt_filename).drop(columns="Filename")
+        stepCnt['interact_id'] = int(interact_id)
+        stepCnt['sd_id'] = int(sd_id)
+        stepCnt['utcdate'] = pd.to_datetime(stepCnt['Time'], format="%Y-%m-%d %H:%M:%S", utc=True)
+        stepCntAdj = pd.read_csv(stepCntAdj_filename).drop(columns="Filename")
+        stepCntAdj['interact_id'] = int(interact_id)
+        stepCntAdj['sd_id'] = int(sd_id)
+        stepCntAdj['utcdate'] = pd.to_datetime(stepCnt['Time'], format="%Y-%m-%d %H:%M:%S", utc=True)
+    except Exception as e:
+        logger.error(f'Unable to format steps from {os.path.basename(axl_elite_filename)}, skipping')
+        return (city_code, wave, os.path.basename(axl_elite_filename), 0, f'Error formatting steps ({e})')
+
 
     # Push to temp table and update table
-    top_con = create_engine(f'postgresql://{db_user}@{db_host}/interact_db')
+    try:
+        top_con = create_engine(f'postgresql://{db_user}@{db_host}/interact_db')
 
-    tmp_step_table = f'sc_{interact_id}_{sd_id}'
-    tmp_stepAdj_table = f'sca_{interact_id}_{sd_id}'
-    stepCnt.to_sql(name=tmp_step_table, con=top_con, if_exists='replace')
-    stepCntAdj.to_sql(name=tmp_stepAdj_table, con=top_con, if_exists='replace')
+        tmp_step_table = f'sc_{interact_id}_{sd_id}'
+        tmp_stepAdj_table = f'sca_{interact_id}_{sd_id}'
+        stepCnt.to_sql(name=tmp_step_table, con=top_con, if_exists='replace')
+        stepCntAdj.to_sql(name=tmp_stepAdj_table, con=top_con, if_exists='replace')
 
-    target_schema = f'top_sd{"" if wave == 1 else wave}'
-    target_table1min = f'top_1min_{city_code}'
-    stmt_update_top1min = f"""
-        UPDATE {target_schema}.{target_table1min} AS top
-            SET steps = sc."Steps",
-                enmo = sc."ENMO(mg)",
-                steps_adj = sca."Steps",
-                enmo_adj = sca."ENMO(mg)"
-            FROM {tmp_step_table} AS sc, {tmp_stepAdj_table} sca
-            WHERE (sc.interact_id, sc.sd_id, sc.utcdate) = (top.interact_id, top.sd_id, top.utcdate)
-                AND (sca.interact_id, sca.sd_id, sca.utcdate) = (top.interact_id, top.sd_id, top.utcdate)
-    """
-    stmt_cleanup_step = f"""
-        DROP TABLE IF EXISTS {tmp_step_table}, {tmp_stepAdj_table}
-    """
+        target_schema = f'top_sd{"" if wave == 1 else wave}'
+        target_table1min = f'top_1min_{city_code}'
+        stmt_update_top1min = f"""
+            UPDATE {target_schema}.{target_table1min} AS top
+                SET steps = sc."Steps",
+                    enmo = sc."ENMO(mg)",
+                    steps_adj = sca."Steps",
+                    enmo_adj = sca."ENMO(mg)"
+                FROM {tmp_step_table} AS sc, {tmp_stepAdj_table} sca
+                WHERE (sc.interact_id, sc.sd_id, sc.utcdate) = (top.interact_id, top.sd_id, top.utcdate)
+                    AND (sca.interact_id, sca.sd_id, sca.utcdate) = (top.interact_id, top.sd_id, top.utcdate)
+        """
+        stmt_cleanup_step = f"""
+            DROP TABLE IF EXISTS {tmp_step_table}, {tmp_stepAdj_table}
+        """
 
-    with top_con.begin() as conn:
-        conn.execute(text(stmt_update_top1min))
-        conn.execute(text(stmt_cleanup_step))
+        with top_con.begin() as conn:
+            conn.execute(text(stmt_update_top1min))
+    except Exception as e:
+        logger.error(f'Unable to save steps to DB from {os.path.basename(axl_elite_filename)}, skipping')
+        return (city_code, wave, os.path.basename(axl_elite_filename), 0, f'Error saving steps to DB ({e})')
+
+    try:
+        with top_con.begin() as conn:
+            conn.execute(text(stmt_cleanup_step))
+    except Exception as e:
+        logger.warning(f'Unable to clean up DB after processing')
+        return (city_code, wave, os.path.basename(axl_elite_filename), 1, f'Warning: unable to cleanup database ({e})')
+
+    t1 = perf_counter() - c0
+    logger.debug(f'PID {mp.current_process().pid}: {t1:.2f}s @ {t1 * 1000 / len(stepCnt.index):.1f}ms/row')
+    return (city_code, wave, os.path.basename(axl_elite_filename), 1, 'Ok')
     
 
 def _etl_axl(axl_elite_filename:str) -> pd.DataFrame:
@@ -226,8 +258,89 @@ def _etl_axl(axl_elite_filename:str) -> pd.DataFrame:
     return tmpf.name
 
 
+def step_produce_sd(src_dir, ncpu=None):
+    """ Batch process all SenseDoc Elite files, which are a pair of CSV files 
+    with raw GPS and AXL data.
+    Data is expected to have been validated beforehand and follow
+    the directory hierarchy defined in ReadMe file:
+        <CITY>
+          |
+          +- <WAVE_N>
+               |
+               +- <sensedoc_elite_files>
+                    |
+                    +- AXL elite files
+    
+    Steps:
+    1. Create schemas/tables in database if required as well as <sensedoc_top_files> 
+        subfolder within each city/wave
+    2. Scan elite file subfolder and store in pool worker argument list
+    3. Run multiprocessing pool of workers
+    4. Report back
+    """
+    # Store pool worker arguments in list of tuples
+    # Arg = (city_code, wave, root_elite_filename, overwrite) / see single_top_produce
+    wrk_args = set()
+
+    src_dir = os.path.abspath(src_dir)
+    for ccode, city in cities.items():
+        for wave in waves:
+            # Modify the top with required fileds
+            execute_alter_top(ccode, wave)
+
+            # Check that city/wave folder exists, which is the case with test data...
+            elite_folder = os.path.join(src_dir, city, f'wave_{wave:02d}', 'sensedoc_elite_files')
+            if not os.path.exists(elite_folder):
+                logger.warning(f'Unable to find elite subfolder <{os.path.relpath(elite_folder, src_dir)}>, skipping')
+                continue
+
+            # Found a folder, all content will be scan to add AXL files to queue
+            with os.scandir(elite_folder) as it:
+                for f in it:
+                    # Check if file is AXL
+                    if re.match('\\d{9}_\\d{1,3}_AXL.csv', f.name) is not None:
+                        axl_elite_fname = os.path.abspath(f.path)
+                        wrk_args.add((ccode, wave, axl_elite_fname, None))
+                                    
+    # Multiprocessing run
+    c0 = perf_counter()
+    if ncpu > 1: # Switch to multiprocessing if more than 1 CPU
+        logger.info(f'Multiprocessing with {ncpu} cores')
+        with mp.Pool(processes=ncpu, maxtasksperchild=1) as pool:
+            results = pool.starmap_async(single_step_produce, wrk_args)
+            result_df = pd.DataFrame([r for r in results.get()], columns=['City', 'Wave', 'Filename', 'Status', 'Details']).convert_dtypes()
+    else:
+        # Single thread processing (for debug only)
+        results = starmap(single_step_produce, wrk_args)
+        result_df = pd.DataFrame([r for r in results], columns=['City', 'Wave', 'Filename', 'Status', 'Details']).convert_dtypes()
+
+    print(result_df)
+
+    # Display stats on computation
+    print('==== PROCESSING REPORT | Steps ====')
+    result_steps_df = result_df.groupby(['City', 'Wave', 'Status'], as_index=False).size()
+    result_steps_df.loc[result_steps_df['Status'] == 0,'Step Status'] = 'Error'
+    result_steps_df.loc[result_steps_df['Status'] == 1,'Step Status'] = 'OK'
+    result_steps_df.loc[result_steps_df['Status'] == -1,'Step Status'] = 'Skipped'
+    result_steps_df = result_steps_df.pivot(index=['City', 'Wave'], columns='Step Status', values='size').fillna(0).convert_dtypes()
+    print(result_steps_df.reset_index().to_markdown(index=False, tablefmt='presto'))
+    print(f'DONE: {perf_counter() - c0:.1f}s')
+
+
 if __name__ == '__main__':
-    execute_alter_top('mtl', 1)
-    single_step_produce('mtl', 1, 
-                        r'data\interact_test_data\montreal\wave_01\sensedoc_elite_files\401102101_60_AXL.csv',
-                        r'data/output_step')
+    # execute_alter_top('vic', 2)
+    # single_step_produce('vic', 2, 
+    #                     #r'data\interact_test_data\montreal\wave_01\sensedoc_elite_files\401102101_60_AXL.csv',
+    #                     r'data\interact_test_data\victoria\wave_02\sensedoc_elite_files\102696608_464_AXL.csv',
+    #                     r'data/output_step')
+
+    # Get target root folder as command line argument
+    if len(sys.argv[1:]):
+        root_data_folder = sys.argv[1]
+
+    if not os.path.isdir(root_data_folder):
+        logger.error(f'No directory <{root_data_folder}> found! Aborting')
+        exit(1)
+
+    ncpus = int(os.environ.get('SLURM_CPUS_PER_TASK',default=6))
+    step_produce_sd(root_data_folder, ncpus)
