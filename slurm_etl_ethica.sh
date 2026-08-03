1bc0590084a70f728b292ce4ea6fcad70bb127e0
Benoit Thierry <benoit.thierry@umontreal.ca>
2026-08-03 13:20:23 -0700

    Switched to newer Pip packages to fix vulnerabilities

--- slurm_etl_ethica.sh
+++ slurm_etl_ethica.sh
@@ -3,17 +3,18 @@
 #SBATCH --mem-per-cpu=24G
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=12:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mail-user=benoit.thierry@umontreal.ca
 #SBATCH --mail-type=ALL
 
-module load StdEnv/2023 python/3.11 scipy-stack/2023b arrow/25.0.0
+module load StdEnv/2023 python/3.11 scipy-stack/2026a arrow/25.0.0
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 module load proj # Need to be loaded once venv is activated
 pip install --no-index --upgrade pip
 # pip install -r requirements.txt
 pip install --no-index tabulate SQLAlchemy resampy psycopg2 polars
+pip freeze > requirements_ethica_slurm.txt
 # python /home/btcrchum/projects/def-dfuller/btcrchum/data_pipeline/ethica/ETL/validate.py /home/btcrchum/projects/def-dfuller/interact/data_archive
-python /home/btcrchum/projects/def-dfuller/btcrchum/data_pipeline/ethica/ETL/load.py /home/btcrchum/projects/def-dfuller/interact/data_archive
+# python /home/btcrchum/projects/def-dfuller/btcrchum/data_pipeline/ethica/ETL/load.py /home/btcrchum/projects/def-dfuller/interact/data_archive 4
 # python /home/btcrchum/projects/def-dfuller/btcrchum/data_pipeline/ethica/ETL/top.py /home/btcrchum/projects/def-dfuller/interact/data_archive 4
